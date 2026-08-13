e2ee4e896fd5cdfb301d872ac84a960a38f5019e
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-13 18:35:12 +0530

    setup axios and authAction for login

--- src/features/auth/state/auth/authAction.jsx
+++ src/features/auth/state/auth/authAction.jsx
@@ -1,1 +1,16 @@
-let 
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { axiosInstance } from "../../../../config/axiosinstance";
+
+export let loginEmployee = createAsyncThunk(
+  "auth/login",
+  async (credentials, thunkApi) => {
+    try {
+      let res = await axiosInstance.post("/auth/login", credentials);
+      console.log(res);
+
+      return res.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error);
+    }
+  },
+);
