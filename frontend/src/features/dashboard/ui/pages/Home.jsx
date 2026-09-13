51c1cd7f59002a30e52fa708bd797b14272c1ee2
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-09-13 19:50:17 +0530

    dashboardlayout process

--- frontend/src/features/dashboard/ui/pages/Home.jsx
+++ frontend/src/features/dashboard/ui/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggletheme } from "../../../../shared/state/themeSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const handleThemeChange = () => {
+    dispatch(toggletheme());
+  };
   return (
     <div>
       <h1>This is my home dashboard page</h1>
+      <button onClick={handleThemeChange}>Change theme</button>
     </div>
   );
 };
--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Outlet } from "react-router";
 
 const DashboardLayout = () => {
+  const { mode } = useSelector((store) => store.theme);
+  useEffect(() => {
+    if (mode === "light") {
+      document.body.classList.add("light");
+    } else {
+      document.body.classList.remove("light");
+    }
+  }, [mode]);
   return (
-    <div>
-      <h1>This is navbar</h1>
-      <Outlet />
+    <div className="h-screen grid grid-cols-[1fr_7fr">
+      <div className="border-r border-gray-500 p-4">navbar</div>
+      <div className="flex flex-col gap-5 p-4">
+        <div>top nav</div>
+        <Outlet />
+      </div>
     </div>
   );
 };
