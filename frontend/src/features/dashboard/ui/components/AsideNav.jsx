bb60ca204f4d970fd3e78476d26f09f8fc4889bb
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-09-15 13:45:38 +0530

    processing

--- frontend/src/features/dashboard/ui/components/AsideNav.jsx
+++ frontend/src/features/dashboard/ui/components/AsideNav.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 const AsideNav = () => {
   return (
     <div>
       <div className="flex flex-col gap-1">
         <h1 className="text-3xl font-semibold text-[#CAB8F9]">team-sync</h1>
-        <p className="text-sm text-(--text-secondary)">
-          Enterprise Workspace
-        </p>
+        <p className="text-sm text-(--text-secondary)">Enterprise Workspace</p>
+      </div>
+      <div>
+        <NavLink to={"/home/department"}>Department</NavLink>
       </div>
     </div>
   );
